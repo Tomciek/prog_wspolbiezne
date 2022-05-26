59d2436190c93ed5355bee47f8fe3639400585d2
Tomasz Lesiak <101012040+Tomciek@users.noreply.github.com>
2022-05-26 21:41:12 +0200

    Update LogicAbstractAPITest.cs

--- prog_wpolbiezne/LogicTests/LogicAbstractAPITest.cs
+++ prog_wpolbiezne/LogicTests/LogicAbstractAPITest.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Data;
 using Logic;
 using System.Collections.ObjectModel;
@@ -23,23 +23,5 @@ namespace Tests
         }
 
         DataAbstractAPI _layer = DataAbstractAPI.Create();
-
-        ObservableCollection<Circle> _newCircles = new();
-        Circle circle1 = new(10, 2, 4);
-        Circle circle2 = new(15, 6, 5);
-
-        [TestMethod]
-        public void CreateCirclesTest()
-        {
-            Assert.AreEqual(3, logicApi.CreateCircles(3, 100, 100).Count);
-        }
-
-        [TestMethod]
-        public void UpdateCirclesTest()
-        {
-            _newCircles.Add(circle1);
-            _newCircles.Add(circle2);
-            Assert.AreEqual(2, logicApi.UpdatePositions(100, 100, _newCircles).Count);
-        }
     }
 }
