4780f762c805c7d43308674c860b6d38501c8ccd
Tomasz Lesiak <101012040+Tomciek@users.noreply.github.com>
2022-05-26 21:36:57 +0200

    Update DataAPI.cs

--- prog_wpolbiezne/Data/DataAPI.cs
+++ prog_wpolbiezne/Data/DataAPI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,41 @@ namespace Data
 {
     internal class DataAPI : DataAbstractAPI
     {
+        private Pool pool;
+
         public override void Connect()
         {
             throw new NotImplementedException();
+        }
+
+        public override void CreatePoolWithBalls(int amount, double widthOfCanvas, double heightOfCanvas)
+        {
+            this.pool = new Pool(amount, widthOfCanvas, heightOfCanvas);
+        }
+
+        public override List<Circle> GetCircles()
+        {
+            return pool.GetCircles();
+        }
+
+        public override void InterruptThreads()
+        {
+            pool.InterruptThreads();
+        }
+
+        public override void StartThreads()
+        {
+            pool.StartThreads();
+        }
+
+        public override double GetPoolHeight()
+        {
+            return pool.GetPoolHeight();
+        }
+
+        public override double GetPoolWidth()
+        {
+            return pool.GetPoolWidth();
         }
     }
 }
