7f3d524355c084e9bf8783af538156bdaaf22693
Tomasz Lesiak <101012040+Tomciek@users.noreply.github.com>
2022-06-14 19:15:01 +0200

    Update Logger.cs

--- prog_wpolbiezne/Data/Logger.cs
+++ prog_wpolbiezne/Data/Logger.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    internal class Logger : IDisposable
+    {
+
+        private Logger()
+        {
+            this.file = new($"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName}" +
+                $"/ballsData.yaml", append: true);
+        }
+        private StreamWriter file;
+        private static Logger _instance;
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+
+        public void SaveDataAsYaml(CircleInformation obj)
+        {
+
+            file.Write(obj.ToString());
+        }
+
+        public void Dispose()
+        {
+            file.Close();
+        }
+    }
+}
